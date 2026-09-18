c5bde8019e3941e39b7338d6ff879426c8200814
kailua <kailua@project2by2.jp>
2026-09-19 01:12:02 +0900

    SX3: Set use_pre_baklava_qpr0_struct for wifi

--- device.mk
+++ device.mk
@@ -355,7 +355,7 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.mediatek
 
 # WiFi
-$(call soong_config_set_bool,mediatek_wifi_hal,use_pre_u_qpr2_struct,true)
+$(call soong_config_set_bool,mediatek_wifi_hal,use_pre_baklava_qpr0_struct,true)
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
