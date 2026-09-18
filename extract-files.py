ade370a75f2fb404c7e3749e98fc11290d4587ba
kailua <kailua@project2by2.jp>
2026-09-18 22:03:26 +0900

    fix checkelf 2

--- extract-files.py
+++ extract-files.py
@@ -116,7 +116,9 @@ blob_fixups: blob_fixups_user_type = {
      'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V7-ndk.so'): blob_fixup()
         .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V7-ndk.so')
         .replace_needed('android.hardware.graphics.allocator-V1-ndk.so', 'android.hardware.graphics.allocator-V2-ndk.so'),
-    ('vendor/lib64/libneuralnetworks_sl_driver_mtk_legacy_prebuilt.so', 'vendor/lib64/libANCBeauty.so'): blob_fixup()
+    ('vendor/lib64/mt6833/libneuron_adapter_mgvi.so',
+     'vendor/lib64/libANCBeauty.so',
+     'vendor/lib64/libneuralnetworks_sl_driver_mtk_legacy_prebuilt.so'): blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_createFromHandle')
         .clear_symbol_version('AHardwareBuffer_describe')
